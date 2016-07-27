6cf38dac1a1b42358303ab38c808447ac3eb0c86
Ishmeet Singh Sethi <sethi.ishmeet@gmail.com>
2016-07-26 21:45:13 -0400

    Initial Commit

---
+++ ISModalPresentation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISModalPresentation
+//
+//  Created by Ishmeet Singh Sethi on 2016-07-26.
+//  Copyright © 2016 Ishmeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
